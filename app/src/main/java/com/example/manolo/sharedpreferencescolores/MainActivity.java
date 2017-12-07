461d071421e011d4211d2c3610abfb1ded79fdfc
wolfBonobo <wolfBonobo@users.noreply.github.com>
2017-12-07 16:21:53 +0100

    Actualizo

--- app/src/main/java/com/example/manolo/sharedpreferencescolores/MainActivity.java
+++ app/src/main/java/com/example/manolo/sharedpreferencescolores/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.manolo.sharedpreferencescolores;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
@@ -13,9 +16,10 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener, View.OnClickListener {
 
-    SeekBar sbRojo, sbVerde, sbAzul;
-    TextView tvRojo, tvVerde, tvAzul;
+    SeekBar sbRojo, sbVerde, sbAzul,sbT;
+    TextView tvRojo, tvVerde, tvAzul,tvT;
     Button btnSharedPreferences;
+    SurfaceView svaGuardado,svaActual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,10 +29,15 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         sbRojo=(SeekBar)findViewById(R.id.sbRojo);
         sbVerde=(SeekBar)findViewById(R.id.sbVerde);
         sbAzul=(SeekBar)findViewById(R.id.sbAzul);
+        sbT=(SeekBar)findViewById(R.id.sbT);
 
         tvRojo=(TextView)findViewById(R.id.tvRojo);
         tvVerde=(TextView)findViewById(R.id.tvVerde);
         tvAzul=(TextView)findViewById(R.id.tvAzul);
+        tvT=(TextView)findViewById(R.id.tvT);
+
+        svaGuardado =(SurfaceView)findViewById(R.id.sfvGuardado);
+        svaActual =(SurfaceView)findViewById(R.id.sfvActual);
 
         btnSharedPreferences=(Button)findViewById(R.id.btnSharedPreferences);
 
@@ -36,18 +45,26 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         sbRojo.setOnSeekBarChangeListener(this);
         sbVerde.setOnSeekBarChangeListener(this);
         sbAzul.setOnSeekBarChangeListener(this);
+        sbT.setOnSeekBarChangeListener(this);
         btnSharedPreferences.setOnClickListener(this);
 
         //Incorporamos Objeto SharedPreferences para la persistencia de la informacion
         SharedPreferences prefe=getSharedPreferences("datos", Context.MODE_PRIVATE);
-        tvRojo.setText(prefe.getString("rojo", ""));
-        tvVerde.setText(prefe.getString("verde", ""));
-        tvAzul.setText(prefe.getString("azul", ""));
+        tvRojo.setText(prefe.getString("rojo", "0"));
+        tvVerde.setText(prefe.getString("verde", "0"));
+        tvAzul.setText(prefe.getString("azul", "0"));
+        tvT.setText(prefe.getString("opaco", "0"));
 
         //Ajustamos las SeekBars al valor cargado por las SharedPreferences
         sbRojo.setProgress(Integer.parseInt(tvRojo.getText().toString()));
         sbVerde.setProgress(Integer.parseInt(tvVerde.getText().toString()));
         sbAzul.setProgress(Integer.parseInt(tvAzul.getText().toString()));
+        sbT.setProgress(Integer.parseInt(tvT.getText().toString()));
+        int opacidad=Integer.parseInt(tvT.getText().toString());
+        int rojo=Integer.parseInt(tvRojo.getText().toString());
+        int verde=Integer.parseInt(tvVerde.getText().toString());
+        int azul=Integer.parseInt(tvAzul.getText().toString());
+        svaGuardado.setBackgroundColor(Color.argb(opacidad,rojo, verde, azul));
 
     }
 
@@ -61,6 +78,15 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
             tvVerde.setText(new Integer(i).toString());
         if(id==sbAzul.getId())
             tvAzul.setText(new Integer(i).toString());
+        if(id==sbT.getId())
+            tvT.setText(new Integer(i).toString());
+        int rojo=Integer.parseInt(tvRojo.getText().toString());
+        int verde=Integer.parseInt(tvVerde.getText().toString());
+        int azul=Integer.parseInt(tvAzul.getText().toString());
+        int opacidad=Integer.parseInt(tvT.getText().toString());
+       // int color=Integer.parseInt(tvRojo.getText().toString())+Integer.parseInt(tvVerde.getText().toString())+Integer.parseInt(tvAzul.getText().toString());
+
+        svaActual.setBackgroundColor(Color.argb(opacidad,rojo, verde, azul));
     }
 
     @Override
@@ -82,5 +108,9 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         editor.putString("azul", tvAzul.getText().toString());
         editor.commit();
         Toast.makeText(this, "Se ha guardado el color elegido", Toast.LENGTH_LONG).show();
+        int rojo=Integer.parseInt(tvRojo.getText().toString());
+        int verde=Integer.parseInt(tvVerde.getText().toString());
+        int azul=Integer.parseInt(tvAzul.getText().toString());
+        svaGuardado.setBackgroundColor(Color.rgb(rojo, verde, azul));
     }
 }
