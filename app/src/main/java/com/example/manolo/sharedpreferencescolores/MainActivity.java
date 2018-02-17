4355481b8229555411bed5b1ca919925a5220205
wolfBonobo <wolfBonobo@users.noreply.github.com>
2018-02-17 17:53:25 +0100

    Código comentado.

--- app/src/main/java/com/example/manolo/sharedpreferencescolores/MainActivity.java
+++ app/src/main/java/com/example/manolo/sharedpreferencescolores/MainActivity.java
@@ -26,40 +26,45 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sbRojo=(SeekBar)findViewById(R.id.sbRojo);
-        sbVerde=(SeekBar)findViewById(R.id.sbVerde);
-        sbAzul=(SeekBar)findViewById(R.id.sbAzul);
-        sbT=(SeekBar)findViewById(R.id.sbT);
+        sbRojo=findViewById(R.id.sbRojo);
+        sbVerde=findViewById(R.id.sbVerde);
+        sbAzul=findViewById(R.id.sbAzul);
+        sbT=findViewById(R.id.sbT);
 
-        tvRojo=(TextView)findViewById(R.id.tvRojo);
-        tvVerde=(TextView)findViewById(R.id.tvVerde);
-        tvAzul=(TextView)findViewById(R.id.tvAzul);
-        tvT=(TextView)findViewById(R.id.tvT);
+        tvRojo=findViewById(R.id.tvRojo);
+        tvVerde=findViewById(R.id.tvVerde);
+        tvAzul=findViewById(R.id.tvAzul);
+        tvT=findViewById(R.id.tvT);
 
-        svaGuardado =(SurfaceView)findViewById(R.id.sfvGuardado);
-        svaActual =(SurfaceView)findViewById(R.id.sfvActual);
+        svaGuardado =findViewById(R.id.sfvGuardado);
+        svaActual =findViewById(R.id.sfvActual);
 
-        btnSharedPreferences=(Button)findViewById(R.id.btnSharedPreferences);
+        btnSharedPreferences=findViewById(R.id.btnSharedPreferences);
 
-        //Agregamos escuchadores
+        //Agregamos escuchadores/Eventos a SeekBars
         sbRojo.setOnSeekBarChangeListener(this);
         sbVerde.setOnSeekBarChangeListener(this);
         sbAzul.setOnSeekBarChangeListener(this);
         sbT.setOnSeekBarChangeListener(this);
         btnSharedPreferences.setOnClickListener(this);
 
+        //SHARED PREFERENCES
         //Incorporamos Objeto SharedPreferences para la persistencia de la informacion
+        //Clave valor. Siempre poner un datos entre comillas para evitar exceptions
         SharedPreferences prefe=getSharedPreferences("datos", Context.MODE_PRIVATE);
         tvRojo.setText(prefe.getString("rojo", "0"));
         tvVerde.setText(prefe.getString("verde", "0"));
         tvAzul.setText(prefe.getString("azul", "0"));
-        tvT.setText(prefe.getString("opaco", "0"));
+        tvT.setText(prefe.getString("opaco", "250"));
 
         //Ajustamos las SeekBars al valor cargado por las SharedPreferences
         sbRojo.setProgress(Integer.parseInt(tvRojo.getText().toString()));
         sbVerde.setProgress(Integer.parseInt(tvVerde.getText().toString()));
         sbAzul.setProgress(Integer.parseInt(tvAzul.getText().toString()));
         sbT.setProgress(Integer.parseInt(tvT.getText().toString()));
+        // svaGuardado.setBackgroundColor(Color.rgb(rojo,verde,azul));
+
+        //PARA OPACIDAD
         int opacidad=Integer.parseInt(tvT.getText().toString());
         int rojo=Integer.parseInt(tvRojo.getText().toString());
         int verde=Integer.parseInt(tvVerde.getText().toString());
@@ -84,8 +89,8 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
         int verde=Integer.parseInt(tvVerde.getText().toString());
         int azul=Integer.parseInt(tvAzul.getText().toString());
         int opacidad=Integer.parseInt(tvT.getText().toString());
-       // int color=Integer.parseInt(tvRojo.getText().toString())+Integer.parseInt(tvVerde.getText().toString())+Integer.parseInt(tvAzul.getText().toString());
-
+      //  svaActual.setBackgroundColor(Color.rgb(opacidad,rojo, verde, azul));
+        //se actualiza el sufaceView co los colores (Opaciodad incluida
         svaActual.setBackgroundColor(Color.argb(opacidad,rojo, verde, azul));
     }
 
@@ -99,18 +104,27 @@ public class MainActivity extends AppCompatActivity implements SeekBar.OnSeekBar
 
     }
 
+    //Evento bonto
     @Override
     public void onClick(View view) {
+        //SE RECUPERA LOS DATOS CUADADOS EN EL MOVIL "datos"
         SharedPreferences preferencias=getSharedPreferences("datos", Context.MODE_PRIVATE);
+        //se indica que lo vamos a editar
         SharedPreferences.Editor editor=preferencias.edit();
+        //Recuperamos valores de los textView y lo añadimos con metodo put
         editor.putString("rojo", tvRojo.getText().toString());
         editor.putString("verde", tvVerde.getText().toString());
         editor.putString("azul", tvAzul.getText().toString());
+        editor.putString("opaco", tvT.getText().toString());
+        //commit
         editor.commit();
         Toast.makeText(this, "Se ha guardado el color elegido", Toast.LENGTH_LONG).show();
+
+        //ACTUALIZAMOS SURFACE VIEW GUARDADO
         int rojo=Integer.parseInt(tvRojo.getText().toString());
         int verde=Integer.parseInt(tvVerde.getText().toString());
         int azul=Integer.parseInt(tvAzul.getText().toString());
-        svaGuardado.setBackgroundColor(Color.rgb(rojo, verde, azul));
+        int opaco=Integer.parseInt(tvT.getText().toString());
+        svaGuardado.setBackgroundColor(Color.argb(opaco,rojo, verde, azul));
     }
 }
